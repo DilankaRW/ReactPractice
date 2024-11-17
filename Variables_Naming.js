e5310d6187cc3f60418132ea8b26f912e6923ade
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-17 23:40:14 +0530

    Variables Naming

---
+++ Variables_Naming.js
@@ -0,0 +1,8 @@
+// Variables Naming
+// * Only Starts with letter, dollar sign($), underscore(_)
+// * Can include letters (a-z,A-Z), digits (0-9), dollar signs($) and underscores (_)
+// * Cannot use reserved words like : 'function', 'var', 'if', 'else', etc
+// * Case-sensitive
+
+var Test = 10;
+console.log(Test);
