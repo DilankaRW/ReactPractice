7feede0bdbee8825485a0e266b325f563840b78e
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-23 23:54:03 +0530

    Template literals - Interpolation

---
+++ Template Literals/interpolation.js
@@ -0,0 +1,7 @@
+const name = "DW";
+const age = 25;
+
+console.log('Hi, My name is '+ name +'.' + 'I am' + age + 'years old.');
+
+// Interpolation
+console.log(`Hi this is ${name}. I am ${age} years old.`)
