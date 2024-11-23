c321ddb1f07a2e9c45e45f7bfdcf4e53bc062506
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-23 23:26:58 +0530

    Template literals - multi line string

---
+++ Template Literals/template.js
@@ -0,0 +1,11 @@
+const text = "This \n\tis \n\t\ta \n\t\t\ttext.";
+
+// Template literals - multi-line string
+
+const text2 = `This
+    is
+        an
+            another
+                text.`;
+console.log(text);
+console.log(text2);
